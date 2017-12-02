6749df52147706155a7b870a64703f8cc79597dc
Nils Hartmann <nils@nilshartmann.net>
2017-12-02 10:42:40 +0100

    Add mongodb

--- brew.sh
+++ brew.sh
@@ -16,6 +16,7 @@ brew install gradle
 brew install yarn --without-node
 brew install httpie
 brew install git bash-completion
+brew install mongodb
 
 # Remove outdated versions from the cellar.
 brew cleanup
